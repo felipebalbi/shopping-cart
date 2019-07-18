18aa8ace548753841a272c0590b97f51beb35a24
Felipe Balbi <balbif@gmail.com>
2019-07-18 10:08:23 +0300

    starting vuex
    Signed-off-by: Felipe Balbi <balbif@gmail.com>

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import Vuex from 'vuex'
+import Vue from 'vue'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    products: []
+  },
+
+  getters: {
+    productsCount () {
+    }
+  },
+
+  actions: {
+    fetchProducts () {
+    }
+  },
+
+  mutations: {
+    setProducts (state, products) {
+      state.products = products
+    }
+  }
+})
